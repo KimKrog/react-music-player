ec128ea627dc9f892c004e273bb67b0d49adc139
Kim Krog <kimkrog@Kims-MBP.localdomain>
2021-03-21 20:43:23 +0100

    Setup data + player functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import Player from './components/Player';
+import Song from './components/Song';
+
+import data from './util';
+
+import './styles/app.scss';
+
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-
+      <Song currentSong={currentSong} />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
     </div>
   );
 }
